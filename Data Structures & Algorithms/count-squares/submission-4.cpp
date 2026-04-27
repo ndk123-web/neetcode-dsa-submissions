e4116c934460807dbf7e267a8ad83b05467156e0
Navnath Kadam <navnathkadam284@gmail.com>
2026-04-27 12:37:16 +0530

    Add: count-squares - submission-4

---
+++ Data Structures & Algorithms/count-squares/submission-4.cpp
@@ -0,0 +1,39 @@
+struct pair_hash {
+    size_t operator()(const pair<int,int>& p) const {
+        return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
+    }
+};
+
+class CountSquares {
+   private:
+    unordered_map<pair<int,int>, int, pair_hash> freqMap;
+    vector<vector<int>> points;
+
+   public:
+    CountSquares() {}
+
+    void add(vector<int> point) {
+        freqMap[{point[0], point[1]}]++;
+        points.push_back(point);
+    }
+
+    int count(vector<int> query) {
+        int count = 0;
+        int x2 = query[0];
+        int y2 = query[1];
+
+        for (vector<int>& point : points) {
+            int x = point[0];
+            int y = point[1];
+
+            // if (x-x2) == (y-y2) means diagonal
+            if (abs(x - x2) == abs(y - y2) && x != x2 && y != y2) {
+
+                // if it is then calculate wrt to freq of each point
+                count += freqMap[{x, y2}] * freqMap[{x2, y}];
+            }
+        }
+
+        return count;
+    }
+};
